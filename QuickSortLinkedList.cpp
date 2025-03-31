b4f860f811a0fb52d07eb7958ebd7eebcc1e80e3
hunNG <giahungcantho11@gmail.com>
2025-03-31 13:39:47 +0700

    abcd
    1234

--- QuickSortLinkedList.cpp
+++ QuickSortLinkedList.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+
 // Cấu trúc của một node
 struct NODE {
 	int info;
@@ -12,20 +13,19 @@ struct LIST {
 	NODE* pHead;
 	NODE* pTail;
 };
-
 NODE* CreateNode(int x) {
-	NODE* p;
+    NODE* p;
 	p = new NODE;
 	//if (p == NULL)
     //		exit(1);
-
+    
 	p->info = x;
 	p->pNext = NULL;
 	return p;
 }
 void AddTail(LIST &L, NODE* p) {
-	if (L.pHead == NULL) {
-		L.pHead = p;
+    if (L.pHead == NULL) {
+        L.pHead = p;
 		L.pTail = L.pHead;
 	}
 	else {
@@ -35,15 +35,15 @@ void AddTail(LIST &L, NODE* p) {
 }
 
 void CreateEmptyList(LIST &L) {
-	L.pHead = NULL;
+    L.pHead = NULL;
 	L.pTail = NULL;
 }
 void CreateList(LIST &L) {
-	NODE* p;
+    NODE* p;
 	int x, n;
     cin >> n; // Số lượng phần tử của danh sách
 	while (n-->0) {
-		cin >> x;
+        cin >> x;
 		p = CreateNode(x);
 		AddTail(L, p);
 	}
@@ -57,8 +57,8 @@ void CreateList(LIST &L) {
 NODE *SeparateHead(LIST &L)
 {
     if (L.pHead == nullptr)
-        return nullptr;
-
+    return nullptr;
+    
     NODE *head = L.pHead;
     L.pHead = L.pHead->pNext;
     head->pNext = nullptr;
@@ -67,9 +67,9 @@ NODE *SeparateHead(LIST &L)
 void Partition(LIST &L, LIST &L1, NODE *&pivot, LIST &L2)
 {
     if (L.pHead == nullptr)
-        return;
+    return;
     pivot = SeparateHead(L);
-
+    
     if (L.pHead == nullptr) return;
     while (L.pHead != nullptr)
     {
@@ -91,9 +91,9 @@ void Join(LIST& L, LIST& L1, NODE* pivot, LIST& L2) {
         L.pHead = L1.pHead;
         L1.pTail->pNext = pivot;
     }
-
+    
     pivot->pNext = L2.pHead;
-
+    
     if (L2.pTail != nullptr) {
         L.pTail = L2.pTail;
     } else {
@@ -105,11 +105,11 @@ void Join(LIST& L, LIST& L1, NODE* pivot, LIST& L2) {
 void quicksort_ascending(LIST&L){
     if (L.pHead == nullptr || L.pHead == L.pTail)
         return;
-    NODE*pivot;
-    LIST L1, L2;
+        NODE*pivot;
+        LIST L1, L2;
     CreateEmptyList(L1);
     CreateEmptyList(L2);
-
+    
     Partition(L,L1,pivot,L2);
     quicksort_ascending(L1);
     quicksort_ascending(L2);
@@ -133,13 +133,13 @@ int length(NODE *p){
 }
 
 void PrintList(NODE* p) {
-	if (p == NULL)
-		cout << "Empty List.";
+    if (p == NULL)
+    cout << "Empty List.";
 	else {
         // cout << boolalpha << is_ascending(p) << endl;
         cout << length(p) << endl;
 		while (p) {
-			cout << p->info << " ";
+            cout << p->info << " ";
 			p = p->pNext;
 		}
 	}
@@ -150,10 +150,13 @@ void PrintList(NODE* p) {
 int main() {
     LIST L;
 	CreateEmptyList(L);
-
+    #ifdef DEBUG
+    std::clog << "abc";
+    #endif
+    
 	CreateList(L);
     quicksort_ascending(L);
     PrintList(L.pHead);
-
+    
     return 0;
 }
